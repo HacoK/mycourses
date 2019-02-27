00a5ec251a1c6dd5cc4d0224648bdb330e12c502
Haco <877728156@qq.com>
2019-02-27 11:25:36 +0800

    update profileTC

---
+++ src/main/java/com/nju/mycourses/config/PathConfig.java
@@ -0,0 +1,9 @@
+package com.nju.mycourses.config;
+
+public class PathConfig {
+    private static String coursewareRootPath="D:mycourses/storage/coursewares/";
+
+    public static String getCoursewareRootPath(){
+        return coursewareRootPath;
+    }
+}
